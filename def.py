aa88d706b6f3dba2c883ceacbbfaac34127cf2a6
lsh0214 <netstat3476@naver.com>
2025-11-03 14:01:33 +0900

    Add files via upload

--- def.py
+++ def.py
@@ -34,7 +34,8 @@ def fetch_page_metadata(url):#반환값은 딕셔너리 형태로 title, favicon
             favicon_url = urljoin(base_url, favicon_href)
         else:
             favicon_url = urljoin(base_url, '/favicon.ico')
-
+        if title=="GitHub · Change is constant. GitHub keeps you ahead.":
+            title="GitHub"
         return {
             'title': title,
             'favicon_url': favicon_url,
@@ -55,10 +56,10 @@ def fetch_page_metadata(url):#반환값은 딕셔너리 형태로 title, favicon
             'favicon_url': None,
             'url': url
         }
-# a=fetch_page_metadata('https://claude.ai/new')
-# print("제목: ",a['title'])
-# print("바비콘: ",a['favicon_url'])
-# print("기존 url",a['url'])
+a=fetch_page_metadata('https://github.com/')
+print("제목: ",a['title'])
+print("바비콘: ",a['favicon_url'])
+print("기존 url",a['url'])
 
 def json_open(file:str)->dict:#입력값( 같은 폴더의 json파일 이름 혹은 절대주소인 str) \ (반환값 딕셔너리 자료형 이름:dict)
     with open(file, "r",encoding='utf-8') as f:
